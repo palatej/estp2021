f777efc58b53d5155fddc11bed4b27a8f1317e7f
Jean Palate <jean.palate@nbb.be>
2021-10-13 13:52:28 +0200

    New examples

---
+++ R files/calendars.R
@@ -0,0 +1,25 @@
+library(rjd3modelling)
+
+e<-easter.dates(2000, 2020)
+
+print(rjd3modelling::td(12, c(1980,1), 11, contrasts = F, groups = c(1,1,1,1,2,2,0)))
+
+print(rjd3modelling::td(12, c(1980,1), 12, contrasts = T, groups = c(1,1,1,1,2,2,0)))
+
+cal<-calendar.new()
+calendar.holiday(cal, "NEWYEAR")
+calendar.holiday(cal, "EASTERMONDAY")
+calendar.holiday(cal, "MAYDAY")
+calendar.holiday(cal, "ASCENSION")
+calendar.holiday(cal, "WHITMONDAY")
+calendar.fixedday(cal, 7, 21)
+calendar.holiday(cal, "ASSUMPTION")
+calendar.holiday(cal, "ALLSAINTDAY")
+calendar.holiday(cal, "ARMISTICE")
+calendar.holiday(cal, "CHRISTMAS")
+print(rjd3modelling::htd(cal, 12, c(1980,1), 24, contrasts = F, groups = c(1,1,1,1,2,2,0)))
+
+print(rjd3modelling::htd(cal, 12, c(1980,1), 24, contrasts = T, groups = c(1,1,1,1,2,2,0)))
+
+ltm<-longTermMean(cal, 12, groups = c(1,1,1,1,2,2,0))
+print(ltm)
--- R files/specs.R
+++ R files/specs.R
@@ -8,3 +8,8 @@ q1<-tramoseats(indprod$M.PROD.C.NSA.I15.DE, spec1)
 q2<-tramoseats(indprod$M.PROD.C.NSA.I15.DE, spec2)
 
 ts.plot(ts.union(q1$final$series[,"sa"], q2$final$series[,"sa"]), col=c("gray", "blue"))
+
+spec3<-tramoseats_spec(spec1, transform.function="Log", usrdef.outliersEnabled = T, usrdef.outliersType = c("AO", "AO", "AO"), 
+                       usrdef.outliersDate = c("2020-03-01", "2020-04-01", "2020-05-01"))
+q3<-tramoseats(indprod$M.PROD.C.NSA.I15.DE, spec3)
+
